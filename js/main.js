08de95c539c02fcaf56326c93c08b6ffdd1d29e6
LiYanZhao <liyanzhao0@gmail.com>
2016-03-16 23:43:07 +0800

    Site updated: 2016-03-16 23:43:07

--- js/main.js
+++ js/main.js
@@ -90,8 +90,8 @@ require([], function (){
             "pulse", "fadeIn","fadeInRight", "flipInX", "lightSpeedIn","rotateInUpLeft", "slideInUp","zoomIn",
             ],
             len = animationNames.length,
-            randomAnimationName = animationNames[Math.ceil(Math.random() * len) - 1];
-
+            //randomAnimationName = animationNames[Math.ceil(Math.random() * len) - 1];
+            randomAnimationName = "zoomIn";
           // ie9 不支持css3 keyframe动画, safari不支持requestAnimationFrame, 不使用随机动画，切回原来的动画
           if (!window.requestAnimationFrame) {
               $('.body-wrap > article').css({opacity: 1});
@@ -143,9 +143,9 @@ require([], function (){
     var colorList = ["#6da336", "#ff945c", "#66CC66", "#99CC99", "#CC6666", "#76becc", "#c99979", "#918597", "#4d4d4d"];
     var id = Math.ceil(Math.random()*(colorList.length-1));
     //PC
-    $("#container .left-col .overlay").css({"background-color": colorList[id],"opacity": .3});
+    $("#container .left-col .overlay").css({"background-color": "#76becc","opacity": .3});
     //移动端
-    $("#container #mobile-nav .overlay").css({"background-color": colorList[id],"opacity": .7});
+    $("#container #mobile-nav .overlay").css({"background-color": "#76becc","opacity": .7});
 
     $("table").wrap("<div class='table-area'></div>");
 });
